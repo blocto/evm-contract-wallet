44c19a9b34379a3f824a332cda09605563e3ad55
kbehouse <kbehouse@gmail.com>
2023-02-20 14:23:10 +0800

    feat: add mainnet deploy scripts

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,60 @@
 require("dotenv").config();
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 
-const CONSISTENT_ADDR_TEST = "0x22F607Abfc0805FdA97b4e9dEC3A7469aF5928d9";
-
+const CONSISTENT_DEPOLY_ADDR = "0xB3c12C9535626cfFef5FE76d5A4e198A5e753FD9";
+// const CONSISTENT_DEPOLY_ADDR = "0x70ebDF2a1C9C7A45141760419c42056E3DDfA690";
+// const CONSISTENT_DEPOLY_ADDR ="0x1Dde3A77975235E6DdB31389c3D599dE67AbA1Ad";
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
+    // mainnet config
+    ethereum: {
+      provider: new HDWalletProvider(
+        process.env.SAME_ADDR_KEY,
+        process.env.ETHEREUM_URL
+      ),
+      network_id: 1,
+      url: process.env.ETHEREUM_URL,
+      from: CONSISTENT_DEPOLY_ADDR,
+      gas: 4000000, // 8M
+      gasPrice: 29800000000, // 37.6 gwei
+    },
+    bsc: {
+      provider: new HDWalletProvider(
+        process.env.SAME_ADDR_KEY,
+        "https://bsc-dataseed2.binance.org/"
+      ),
+      network_id: 56,
+      from: CONSISTENT_DEPOLY_ADDR,
+      gas: 5000000, // 5M
+      gasPrice: 6000000000, // 5 gwei
+    },
+    polygon: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.SAME_ADDR_KEY,
+          `https://rpc.ankr.com/polygon`
+        ),
+      network_id: 137,
+      confirmations: 2,
+      timeoutBlocks: 1000,
+      skipDryRun: true,
+      gas: 8000000, // 8M
+      gasPrice: 367000000000, // 400 gwei, check from https://polygonscan.com/gastracker
+    },
+    avalanche: {
+      provider: new HDWalletProvider(
+        process.env.SAME_ADDR_KEY,
+        "https://api.avax.network/ext/bc/C/rpc"
+      ),
+      network_id: 43114,
+      from: CONSISTENT_DEPOLY_ADDR,
+      gasPrice: 25000000000, // 25 nAVAX
+    },
+
+
+    // testnet config
     sepolia: {
       provider: new HDWalletProvider(
         process.env.SAME_ADDR_KEY,
@@ -14,9 +62,19 @@ module.exports = {
       ),
       network_id: 11155111,
       url: process.env.SEPOLIA_URL,
-      from: CONSISTENT_ADDR_TEST,
+      from: CONSISTENT_DEPOLY_ADDR,
       gas: 8000000, // 8M
-      gasPrice: 2000000000, // 2 gwei
+      // gasPrice: 2000000000, // 2 gwei
+      gasPrice: 32000000000, // 32 gwei
+    },
+    rinkeby: {
+      provider: new HDWalletProvider(
+        process.env.SAME_ADDR_KEY,
+        "https://rinkeby.blocto.app"
+      ),
+      network_id: 4,
+      from: CONSISTENT_DEPOLY_ADDR,
+      gasPrice: 40000000000, // 25 nAVAX
     },
     mumbai: {
       provider: () =>
@@ -37,7 +95,7 @@ module.exports = {
         "https://data-seed-prebsc-1-s3.binance.org:8545"
       ),
       network_id: 97,
-      from: CONSISTENT_ADDR_TEST,
+      from: CONSISTENT_DEPOLY_ADDR,
       gas: 5000000, // 5M
       gasPrice: 10000000000, // 10 gwei
     },
@@ -47,7 +105,7 @@ module.exports = {
         "https://api.avax-test.network/ext/bc/C/rpc"
       ),
       network_id: 43113,
-      from: CONSISTENT_ADDR_TEST,
+      from: CONSISTENT_DEPOLY_ADDR,
       gasPrice: 25000000000, // 25 nAVAX
     },
 
@@ -69,6 +127,13 @@ module.exports = {
       runs: 1000,
     },
   },
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY,
+    polygonscan:  process.env.POLYGONSCAN_API_KEY,
+    bscscan: process.env.BSCSCAN_API_KEY,
+    snowtrace: process.env.SNOWTRACE_API_KEY,
+  },
+  plugins: ['truffle-plugin-verify'],
   mocha: {
     bail: true,
   },
