f580b68bdbbd1a0369ad5a19775165f12b653fcf
kbehouse <kbehouse@gmail.com>
2023-02-22 16:48:03 +0800

    chore: update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 
-const CONSISTENT_DEPOLY_ADDR = "0xB3c12C9535626cfFef5FE76d5A4e198A5e753FD9";
-// const CONSISTENT_DEPOLY_ADDR = "0x70ebDF2a1C9C7A45141760419c42056E3DDfA690";
-// const CONSISTENT_DEPOLY_ADDR ="0x1Dde3A77975235E6DdB31389c3D599dE67AbA1Ad";
+const {SAME_ADDR_KEY, SAME_ADDR, ETHEREUM_URL, SEPOLIA_URL,
+      ETHERSCAN_API_KEY, POLYGONSCAN_API_KEY, BSCSCAN_API_KEY, SNOWTRACE_API_KEY} = process.env;
+
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -11,29 +12,29 @@ module.exports = {
     // mainnet config
     ethereum: {
       provider: new HDWalletProvider(
-        process.env.SAME_ADDR_KEY,
-        process.env.ETHEREUM_URL
+        SAME_ADDR_KEY,
+        ETHEREUM_URL
       ),
       network_id: 1,
-      url: process.env.ETHEREUM_URL,
-      from: CONSISTENT_DEPOLY_ADDR,
-      gas: 4000000, // 8M
-      gasPrice: 29800000000, // 37.6 gwei
+      url: ETHEREUM_URL,
+      from: SAME_ADDR,
+      gas: 4000000, // 4M
+      gasPrice: 29800000000, // 29.8 gwei
     },
     bsc: {
       provider: new HDWalletProvider(
-        process.env.SAME_ADDR_KEY,
+        SAME_ADDR_KEY,
         "https://bsc-dataseed2.binance.org/"
       ),
       network_id: 56,
-      from: CONSISTENT_DEPOLY_ADDR,
+      from: SAME_ADDR,
       gas: 5000000, // 5M
-      gasPrice: 6000000000, // 5 gwei
+      gasPrice: 6000000000, // 6 gwei
     },
     polygon: {
       provider: () =>
         new HDWalletProvider(
-          process.env.SAME_ADDR_KEY,
+          SAME_ADDR_KEY,
           `https://rpc.ankr.com/polygon`
         ),
       network_id: 137,
@@ -41,45 +42,42 @@ module.exports = {
       timeoutBlocks: 1000,
       skipDryRun: true,
       gas: 8000000, // 8M
-      gasPrice: 367000000000, // 400 gwei, check from https://polygonscan.com/gastracker
+      gasPrice: 367000000000, // 367 gwei, check from https://polygonscan.com/gastracker
     },
     avalanche: {
       provider: new HDWalletProvider(
-        process.env.SAME_ADDR_KEY,
+        SAME_ADDR_KEY,
         "https://api.avax.network/ext/bc/C/rpc"
       ),
       network_id: 43114,
-      from: CONSISTENT_DEPOLY_ADDR,
+      from: SAME_ADDR,
       gasPrice: 25000000000, // 25 nAVAX
     },
-
-
     // testnet config
     sepolia: {
       provider: new HDWalletProvider(
-        process.env.SAME_ADDR_KEY,
-        process.env.SEPOLIA_URL
+        SAME_ADDR_KEY,
+        SEPOLIA_URL
       ),
       network_id: 11155111,
-      url: process.env.SEPOLIA_URL,
-      from: CONSISTENT_DEPOLY_ADDR,
+      url: SEPOLIA_URL,
+      from: SAME_ADDR,
       gas: 8000000, // 8M
-      // gasPrice: 2000000000, // 2 gwei
       gasPrice: 32000000000, // 32 gwei
     },
     rinkeby: {
       provider: new HDWalletProvider(
-        process.env.SAME_ADDR_KEY,
+        SAME_ADDR_KEY,
         "https://rinkeby.blocto.app"
       ),
       network_id: 4,
-      from: CONSISTENT_DEPOLY_ADDR,
-      gasPrice: 40000000000, // 25 nAVAX
+      from: SAME_ADDR,
+      gasPrice: 40000000000, // 40 nAVAX
     },
     mumbai: {
       provider: () =>
         new HDWalletProvider(
-          process.env.SAME_ADDR_KEY,
+          SAME_ADDR_KEY,
           `https://polygon-testnet.public.blastapi.io`
         ),
       network_id: 80001,
@@ -91,27 +89,27 @@ module.exports = {
     },
     bsc_testnet: {
       provider: new HDWalletProvider(
-        process.env.SAME_ADDR_KEY,
+        SAME_ADDR_KEY,
         "https://data-seed-prebsc-1-s3.binance.org:8545"
       ),
       network_id: 97,
-      from: CONSISTENT_DEPOLY_ADDR,
+      from: SAME_ADDR,
       gas: 5000000, // 5M
       gasPrice: 10000000000, // 10 gwei
     },
     avalanche_testnet: {
       provider: new HDWalletProvider(
-        process.env.SAME_ADDR_KEY,
+        SAME_ADDR_KEY,
         "https://api.avax-test.network/ext/bc/C/rpc"
       ),
       network_id: 43113,
-      from: CONSISTENT_DEPOLY_ADDR,
+      from: SAME_ADDR,
       gasPrice: 25000000000, // 25 nAVAX
     },
 
     development: {
       provider: new HDWalletProvider(
-        "creek honey mixture input soccer soccer waste tube also pluck hundred already",
+        SAME_ADDR_KEY,
         "http://localhost:7545"
       ),
       host: "127.0.0.1",
@@ -128,10 +126,10 @@ module.exports = {
     },
   },
   api_keys: {
-    etherscan: process.env.ETHERSCAN_API_KEY,
-    polygonscan:  process.env.POLYGONSCAN_API_KEY,
-    bscscan: process.env.BSCSCAN_API_KEY,
-    snowtrace: process.env.SNOWTRACE_API_KEY,
+    etherscan: ETHERSCAN_API_KEY,
+    polygonscan:  POLYGONSCAN_API_KEY,
+    bscscan: BSCSCAN_API_KEY,
+    snowtrace: SNOWTRACE_API_KEY,
   },
   plugins: ['truffle-plugin-verify'],
   mocha: {
